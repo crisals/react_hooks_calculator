836086430a05e6429dfed3a9360c0469706f248c
ewigdev <cris.alarcon@icloud.com>
2020-09-07 11:16:28 -0300

    Numeric buttons and result window are added, css is added.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,8 +2,33 @@ import React, {useState} from 'react';
 import "../css/calculator.css";
 
 const Calculator = () => {
+  /* eslint no-eval: 0 */
 
-    
+    const [input, setInput] = useState("");
+    const buttons = [];
+    [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "."].forEach((item) => {
+  
+        buttons.push(    
+            <button onClick={(e) => { setInput(input + e.target.value); }} value={item} key={item} > 
+                {" "} {item} 
+            </button>
+        );
+    });
+
+    buttons.push(    
+        <button className="clearButton" onClick={() => setInput("")} value=""> AC  </button>
+    );
+
+  return (
+    <div className="contain">
+        <div className="show-result">{input}</div>
+
+        <div className="calc">
+            <div className="digitsButton flex">{buttons}</div>
+            
+        </div>
+    </div>
+  );
 }
  
 export default Calculator;
